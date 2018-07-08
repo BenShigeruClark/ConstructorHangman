2523e65665584207d7c7cfc21bfac03461ffd090
Ben Clark <benclarkphoto@mac.com>
2018-07-07 22:50:56 -0700

    https://github.com/BenShigeruClark/ConstructorHangman

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+var inquirer = require("inquirer");
+
+var isLetter = require("is-letter");
+
+var Word = require("./Word.js");
+
+var prompt = require("prompt");
+
+
+
+console.log("Time for Some CONSTRUCTOR HANGMAN!");
+console.log("Guess the letter of the name of a Musical Instrument");
+console.log("Have Fun!");
+console.log("-------------------------------------");
+prompt.start();
+
+// Create a variable for word bank
+
+// create a function that will choose words randomly from the word bank
+
+
+//Create a constructor function that starts the game
+
+//Create a constructor function that alerts user if letter is correct or wrog
+
+//Create a constructor function that will display the letter chosen if wrong
+
+//Create a constructor function that will keep letter in answer space if correct
+
+//Create a constructor function that will give an amount of guesses to user
+
+//Create a constructor function that will display the number of guesses left before game ends
+
+// Create a constructor function that will give user points if word is guessed correctly
+
+// Create a prompt that will alert user to choose another letter
+
+// Create a constructor function and prompt user that they've guess correctly 
+// and that they should pick another letter
+
+// Create a constructor function and prompt user that they've guessed wrong
+// and that they should pick another letter
+
+//  Create a prompt that tells user they've had too many guesses and game is over.
+
+
 /* Randomely selects a word and uses the Word constructor to store 
    it. */
 
